b56129adcd63a3bbd80cc64cf83c87e7ec0c7209
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-18 11:35:01 -0700

    using field types C#

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
 ﻿namespace CsharpFields;
 
+
+
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var customer = new Customer(1);
+        customer.Orders.Add(new Order());
+        customer.Orders.Add(new Order());
+
+        customer.Promote();
+
+        Console.WriteLine(customer.Orders.Count);
+
     }
 }
 
